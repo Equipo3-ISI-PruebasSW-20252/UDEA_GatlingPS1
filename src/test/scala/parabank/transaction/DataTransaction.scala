9403ae81b1bea9dc64f50c91fd00cfe56a4be6e8
juandcalderon16 <106625250+juandcalderon16@users.noreply.github.com>
2025-10-12 10:48:52 -0500

    Update account IDs in DataTransaction object

--- src/test/scala/parabank/transaction/DataTransaction.scala
+++ src/test/scala/parabank/transaction/DataTransaction.scala
@@ -1,7 +1,7 @@
 package parabank.transaction
 
 object DataTransaction {
-  val fromAccountId = "13788"
-  val toAccountId = "21225"
+  val fromAccountId = "13011"
+  val toAccountId = "12345"
   val amount = "0.50"
 }
