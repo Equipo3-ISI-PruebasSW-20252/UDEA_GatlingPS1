62e69c654819cbe81bd9e2357ddda0e7676de9f6
juandcalderon16 <106625250+juandcalderon16@users.noreply.github.com>
2025-10-12 10:46:08 -0500

    Update customer and account IDs in LoanData

--- src/test/scala/parabank/loan/LoanData.scala
+++ src/test/scala/parabank/loan/LoanData.scala
@@ -3,8 +3,8 @@ package parabank.loan
 import scala.util.Random
 
 object LoanData {
-    val customerId = "12767"
-    val fromAccountId = "13788"
+    val customerId = "12212"
+    val fromAccountId = "13011"
     val amount = 1000 + scala.util.Random.nextInt(9000) // between 1000 and 10000
     val downPayment = 100 + scala.util.Random.nextInt(900) // between 100 and 1000
 }
